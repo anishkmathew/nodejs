d3b29821c98a25e26fee4a7036a407fbecc02c3e
anishkmathew <anishkmathew@gmail.com>
2018-06-17 01:33:06 +0530

    Events Basics

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const logger = require('./logger');
+/*const logger = require('./logger');
 logger.log('Test');
-logger.greetings('Anish Mathew');
+logger.greetings('Anish Mathew');*/
+const EventEmitter = require('events');
+var emitter = new EventEmitter();
+
+//Register a Listener
+emitter.on('msgLog',()=>{
+  console.log("Listener called");
+})
+
+//Raise an event
+emitter.emit('msgLog');
