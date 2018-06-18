692b00e6c9313b52b3c3ec0a77f0094688a611b5
anishkmathew <anishkmathew@gmail.com>
2018-06-18 12:32:57 +0530

    Os module

--- app.js
+++ app.js
@@ -16,3 +16,10 @@ emitter.emit('msgLog');
 const path = require('path');
 var pathObj = path.parse(__filename);
 console.log(pathObj);
+
+//OS module
+const os = require('os');
+var totalMemory = os.totalmem();
+var freeMemory  = os.freemem();
+console.log(`TotalMemory : ${totalMemory}`);
+console.log(`freeMemory : ${freeMemory}`);
