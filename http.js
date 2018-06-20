29cfbbf5c4fddb5099523b9f9243e4fd3d2b468b
anish <anishkmathew@gmail.com>
2018-06-20 09:37:19 +0530

    HTTP respone

--- http.js
+++ http.js
@@ -1,7 +1,14 @@
 const http = require('http');
-const server = http.createServer();
-server.on('connection',(socket)=>{
-  console.log('New connection');
+const server = http.createServer((req,res)=>{
+  if (req.url = './'){
+    res.write('This is root file called.')
+  }
+  if (req.url = './api/courses'){
+    res.write('List of courses', JSON.stringify(['BCA','MCA','MBA']));
+  }
 });
+/*server.on('connection',(socket)=>{
+  console.log('New connection');
+});*/
 server.listen(3000);
 console.log('listening on port 3000');
